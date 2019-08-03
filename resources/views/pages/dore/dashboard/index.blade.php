e4cac294144aaf0e3f5192c25dc8420cad34ee48
mrizkir <cv.yacanet@nanhekumar.com>
2019-08-03 18:42:21 +0700

    struktur menu

---
+++ resources/views/pages/dore/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.dore.l_main')
+@section('page_title')
+    RINGKASAN DATA
+@endsection
+@section('page_header')
+    RINGKASAN DATA
+@endsection
+@section('page_breadcrumb')
+    <li class="breadcrumb-item active" aria-current="page">RINGKASAN DATA</li>
+@endsection
