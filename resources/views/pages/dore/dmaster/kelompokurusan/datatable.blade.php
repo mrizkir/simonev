5c480157a78646c0eda6d14373496bcc4018f655
jajangtea <za2ng2509@gmail.com>
2019-08-23 13:26:44 +0700

    update button tabel

--- resources/views/pages/dore/dmaster/kelompokurusan/datatable.blade.php
+++ resources/views/pages/dore/dmaster/kelompokurusan/datatable.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-@if (count($data) > 0)
+    @if (count($data) > 0)
     <div class="col-12">
         <div class="card">
             <div class="card-body">
@@ -12,24 +12,39 @@
                                 <th scope="col">NAMA KELOMPOK URUSAN </th>
                                 <th scope="col">KET.</th>
                                 <th scope="col" width="70">TA</th>
+                                <th scope="col" width="70">AKSI</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($data as $key=>$item)
                             <tr>
                                 <th scope="row">
-                                    {{ ($data->currentpage()-1) * $data->perpage() + $key + 1 }}    
-                                </td>                  
+                                    {{ ($data->currentpage()-1) * $data->perpage() + $key + 1 }}
+                                    </td>
                                 <td>{{$item->Kd_Urusan}}</td>
                                 <td>{{$item->Nm_Urusan}}</td>
                                 <td>{{$item->Descr}}</td>
-                                <td>{{$item->TA}}</td>                    
+                                <td>{{$item->TA}}</td>
+                                <td>
+                                    <div class="input-group-append">
+                                        <a href="#" class="btn btn-primary btn-xs mr-sm-2 default">
+                                            <i class="simple-icon-eye"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-primary btn-xs mr-sm-2 default">
+                                            <i class="simple-icon-pencil"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-danger btn-xs default">
+                                            <i class="simple-icon-trash"></i>
+                                        </a>
+                                    </div>
+                                </td>
                             </tr>
                             <tr class="text-center">
-                                <td colspan="5">
+                                <td colspan="6">
                                     <div class="card d-flex flex-row mb-0">
                                         <div class="d-flex flex-grow-2 min-width-zero">
-                                            <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center" style="padding:10px">
+                                            <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center"
+                                                style="padding:10px">
                                                 <div class="w-15 w-xs-100">
                                                     <span class="badge badge-pill badge-secondary">
                                                         <strong>KURSID:</strong>{{$item->KUrsID}}
@@ -47,24 +62,24 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>                                                  
+                                    </div>
                                 </td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
-                    {{$data->links('layouts.dore.l_pagination')}}  
+                    {{$data->links('layouts.dore.l_pagination')}}
                 </div>
             </div>
         </div>
-    </div>       
-@else
+    </div>
+    @else
     <div class="col-12">
         <div class="card">
             <div class="alert alert-info">
                 Belum ada data yang bisa ditampilkan.
-            </div>                
+            </div>
         </div>
     </div>
-@endif            
+    @endif
 </div>
