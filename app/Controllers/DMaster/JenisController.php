9ce18750bf97b8aa98059d0b084d8019721ee017
jajangtea <za2ng2509@gmail.com>
2019-09-23 15:12:06 +0700

    Model dan Controller Jenis

---
+++ app/Controllers/DMaster/JenisController.php
@@ -0,0 +1,338 @@
+<?php
+
+namespace App\Controllers\DMaster;
+
+use App\Controllers\Controller;
+use App\Models\DMaster\JenisModel;
+use Illuminate\Http\Request;
+
+class JenisController extends Controller
+{
+    /**
+     * Membuat sebuah objek
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * collect data from resources for index view
+     *
+     * @return resources
+     */
+    public function populateData($currentpage = 1)
+    {
+        $columns = ['*'];
+        if (!$this->checkStateIsExistSession('jenis', 'orderby')) {
+            $this->putControllerStateSession('jenis', 'orderby', ['column_name' => 'KlpID', 'order' => 'asc']);
+        }
+        $column_order = $this->getControllerStateSession('jenis.orderby', 'column_name');
+        $direction = $this->getControllerStateSession('jenis.orderby', 'order');
+
+        if (!$this->checkStateIsExistSession('global_controller', 'numberRecordPerPage')) {
+            $this->putControllerStateSession('global_controller', 'numberRecordPerPage', 10);
+        }
+        $numberRecordPerPage = $this->getControllerStateSession('global_controller', 'numberRecordPerPage');
+
+        $data = JenisModel::where('TA', \HelperKegiatan::getTahunPenyerapan())
+            ->orderBy($column_order, $direction)
+            ->paginate($numberRecordPerPage, $columns, 'page', $currentpage);
+
+        $data->setPath(route('jenis.index'));
+        return $data;
+    }
+    /**
+     * digunakan untuk mengganti jumlah record per halaman
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function changenumberrecordperpage(Request $request)
+    {
+        $theme = 'dore';
+
+        $numberRecordPerPage = $request->input('numberRecordPerPage');
+        $this->putControllerStateSession('global_controller', 'numberRecordPerPage', $numberRecordPerPage);
+
+        $this->setCurrentPageInsideSession('jenis', 1);
+        $data = $this->populateData();
+
+
+        $datatable = view("pages.$theme.dmaster.jenis.datatable")->with([
+            'page_active' => 'jenis',
+            'search' => $this->getControllerStateSession('jenis', 'search'),
+            'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+            'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+            'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+            'data' => $data
+        ])->render();
+
+        return response()->json(['success' => true, 'datatable' => $datatable], 200);
+    }
+    /**
+     * digunakan untuk mengurutkan record
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function orderby(Request $request)
+    {
+        $theme = 'dore';
+
+        $orderby = $request->input('orderby') == 'asc' ? 'desc' : 'asc';
+        $column = $request->input('column_name');
+        switch ($column) {
+            case 'col-KlpID':
+                $column_name = 'KlpID';
+                break;
+            case 'col-KlpNm':
+                $column_name = 'KlpNm';
+                break;
+            default:
+                $column_name = 'KlpID';
+        }
+        $this->putControllerStateSession('jenis', 'orderby', ['column_name' => $column_name, 'order' => $orderby]);
+
+        $currentpage = $request->has('page') ? $request->get('page') : $this->getCurrentPageInsideSession('jenis');
+        $data = $this->populateData($currentpage);
+        if ($currentpage > $data->lastPage()) {
+            $data = $this->populateData($data->lastPage());
+        }
+        $datatable = view("pages.$theme.dmaster.jenis.datatable")->with([
+            'page_active' => 'jenis',
+            'search' => $this->getControllerStateSession('jenis', 'search'),
+            'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+            'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+            'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+            'data' => $data
+        ])->render();
+
+        return response()->json(['success' => true, 'datatable' => $datatable], 200);
+    }
+    /**
+     * paginate resource in storage called by ajax
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function paginate($id)
+    {
+        $theme = 'dore';
+
+        $this->setCurrentPageInsideSession('jenis', $id);
+        $data = $this->populateData($id);
+        $datatable = view("pages.$theme.dmaster.jenis.datatable")->with([
+            'page_active' => 'jenis',
+            'search' => $this->getControllerStateSession('jenis', 'search'),
+            'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+            'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+            'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+            'data' => $data
+        ])->render();
+        return response()->json(['success' => true, 'datatable' => $datatable], 200);
+    }
+    /**
+     * search resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request)
+    {
+        $theme = 'dore';
+
+        $action = $request->input('action');
+        if ($action == 'reset') {
+            $this->destroyControllerStateSession('jenis', 'search');
+        } else {
+            $kriteria = $request->input('cmbKriteria');
+            $isikriteria = $request->input('txtKriteria');
+            $this->putControllerStateSession('jenis', 'search', ['kriteria' => $kriteria, 'isikriteria' => $isikriteria]);
+        }
+        $this->setCurrentPageInsideSession('jenis', 1);
+        $data = $this->populateData();
+
+        $datatable = view("pages.$theme.dmaster.jenis.datatable")->with([
+            'page_active' => 'jenis',
+            'search' => $this->getControllerStateSession('jenis', 'search'),
+            'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+            'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+            'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+            'data' => $data
+        ])->render();
+
+        return response()->json(['success' => true, 'datatable' => $datatable], 200);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $theme = 'dore';
+
+        $search = $this->getControllerStateSession('jenis', 'search');
+        $currentpage = $request->has('page') ? $request->get('page') : $this->getCurrentPageInsideSession('jenis');
+        $data = $this->populateData($currentpage);
+        if ($currentpage > $data->lastPage()) {
+            $data = $this->populateData($data->lastPage());
+        }
+        $this->setCurrentPageInsideSession('jenis', $data->currentPage());
+
+        return view("pages.$theme.dmaster.jenis.index")->with([
+            'page_active' => 'jenis',
+            'search' => $this->getControllerStateSession('jenis', 'search'),
+            'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+            'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+            'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+            'data' => $data,
+        ]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $theme = 'dore';
+        return view("pages.$theme.dmaster.jenis.create")->with([
+            'page_active' => 'jenis',
+
+        ]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'Kd_Rek_2' => 'required|min:2',
+            'KlpNm' => 'required|min:5',
+            'Descr' => 'required|min:5',
+        ]);
+
+        $jenis = JenisModel::create([
+            'KlpID' => uniqid('uid'),
+            'StrID' => $request->input('StrID'),
+            'Kd_Rek_2' => $request->input('Kd_Rek_2'),
+            'KlpNm' => $request->input('KlpNm'),
+            'Descr' => $request->input('Descr'),
+            'TA' => \HelperKegiatan::getTahunPenyerapan(),
+        ]);
+
+        if ($request->ajax()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data ini telah berhasil disimpan.'
+            ]);
+        } else {
+            return redirect(route('jenis.show', ['uuid' => $jenis->KlpID]))->with('success', 'Data ini telah berhasil disimpan.');
+        }
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function show($uuid)
+    {
+        $theme = 'dore';
+
+        $data = JenisModel::where('KlpID', $uuid)->firstOrFail();
+        if (!is_null($data)) {
+            return view("pages.$theme.dmaster.jenis.show")->with([
+                'page_active' => 'jenis',
+                'data' => $data,
+            ]);
+        }
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($uuid)
+    {
+        $theme = 'dore';
+
+        $data = JenisModel::findOrFail($uuid);
+        if (!is_null($data)) {
+            return view("pages.$theme.dmaster.jenis.edit")->with([
+                'page_active' => 'rkakegiatanmurni',
+                'data' => $data
+            ]);
+        }
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $uuid)
+    {
+        $this->validate($request, [
+            'Kd_Rek_2' => 'required|min:2',
+            'KlpNm' => 'required|min:5',
+            'Descr' => 'required|min:5',
+        ]);
+
+        $jenis = JenisModel::create([
+            'KlpID' => uniqid('uid'),
+            'StrID' => $request->input('StrID'),
+            'Kd_Rek_2' => $request->input('Kd_Rek_2'),
+            'KlpNm' => $request->input('KlpNm'),
+            'Descr' => $request->input('Descr'),
+            'TA' => \HelperKegiatan::getTahunPenyerapan(),
+        ]);
+
+
+        if ($request->ajax()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data ini telah berhasil disimpan.'
+            ]);
+        } else {
+            return redirect(route('jenis.show', ['uuid' => $jenis->ASNID]))->with('success', 'Data ini telah berhasil disimpan.');
+        }
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $uuid)
+    {
+        $theme = 'dore';
+        $jenis = JenisModel::find($uuid);
+
+        $result = $jenis->delete();
+        if ($request->ajax()) {
+            $currentpage = $this->getCurrentPageInsideSession('jenis');
+            $data = $this->populateData($currentpage);
+            if ($currentpage > $data->lastPage()) {
+                $data = $this->populateData($data->lastPage());
+            }
+            $datatable = view("pages.$theme.dmaster.jenis.datatable")->with([
+                'page_active' => 'jenis',
+                'search' => $this->getControllerStateSession('jenis', 'search'),
+                'numberRecordPerPage' => $this->getControllerStateSession('global_controller', 'numberRecordPerPage'),
+                'column_order' => $this->getControllerStateSession('jenis.orderby', 'column_name'),
+                'direction' => $this->getControllerStateSession('jenis.orderby', 'order'),
+                'data' => $data
+            ])->render();
+
+            return response()->json(['success' => true, 'datatable' => $datatable], 200);
+        } else {
+            return redirect(route('jenis.index'))->with('success', "Data ini dengan ($uuid) telah berhasil dihapus.");
+        }
+    }
+}
---
+++ app/Models/DMaster/JenisModel.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models\DMaster;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class JenisModel extends Model
+{
+    use LogsActivity;
+    /**
+     * nama tabel model ini.
+     *
+     * @var string
+     */
+    protected $table = 'tmKlp';
+    /**
+     * primary key tabel ini.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'KlpID';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'KlpID', 'StrID', 'Kd_Rek_2', 'KlpNm', 'Descr', 'TA'
+    ];
+    /**
+     * enable auto_increment.
+     *
+     * @var string
+     */
+    public $incrementing = false;
+    /**
+     * activated timestamps.
+     *
+     * @var string
+     */
+    public $timestamps = true;
+
+    /**
+     * make the model use another name than the default
+     *
+     * @var string
+     */
+    protected static $logName = 'JenisController';
+    /**
+     * log the changed attributes for all these events 
+     */
+    protected static $logAttributes = ['KlpID', 'StrID', 'Kd_Rek_2', 'KlpNm', 'Descr', 'TA'];
+    /**
+     * log changes to all the $fillable attributes of the model
+     */
+}
