5a12842c96cdcd2ee1bb2552a014a237504858fa
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-12-07 14:13:54 +0700

    hide remove maps and config

--- frontend/dist/js/chunk-2d0d7ae1.c1fc53d0.js
+++ frontend/dist/js/chunk-2d0d7ae1.c1fc53d0.js
@@ -1,2 +1,1 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7ae1"],{"789c":function(n,u,c){"use strict";c.r(u);var e,l,a=c("2877"),o={},p=Object(a["a"])(o,e,l,!1,null,null,null);u["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d0d7ae1.c1fc53d0.js.map
